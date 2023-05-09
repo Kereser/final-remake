fe37ea99492b88d0cacddb372800d49e3f9ec6e6
Kereser <Urregoarr00@gmail.com>
2023-05-09 13:49:32 -0500

    Adding dependencies to app and configurations to application.yml

--- app/src/main/java/org/finalremake/app/AppFinalApplication.java
+++ app/src/main/java/org/finalremake/app/AppFinalApplication.java
@@ -3,7 +3,7 @@ package org.finalremake.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.finalremake")
 public class AppFinalApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppFinalApplication.class, args);
