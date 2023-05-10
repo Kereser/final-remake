3e04443f94bf124717095e3fb4cd2cd75735dd21
Kereser <Urregoarr00@gmail.com>
2023-05-10 18:29:02 -0500

    Adding minor changes to annotations

---
+++ app/src/main/java/org/finalremake/AppFinalApplication.java
@@ -0,0 +1,13 @@
+package org.finalremake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// NECESSARY IF DATA IS IN PACKAGE DIFFERENT FROM ORG.FINALREMAKE in this case.
+//@EntityScan(basePackages = "org.finalremake.data.model")
+public class AppFinalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppFinalApplication.class, args);
+    }
+}
