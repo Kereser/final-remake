13cd0601fd3416354373da3e06aad16dd56e183c
Kereser <Urregoarr00@gmail.com>
2023-05-11 11:50:31 -0500

    Adding DeliveryRepository

---
+++ data/src/main/java/org/finalremake/data/model/delivery/DeliveryRepository.java
@@ -0,0 +1,8 @@
+package org.finalremake.data.model.delivery;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
+}
