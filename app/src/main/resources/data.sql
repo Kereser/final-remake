6fadee46a880aecfeab809e8f7a56d246ccb95aa
Kereser <Urregoarr00@gmail.com>
2023-05-11 17:45:17 -0500

    Adding address to data.sql

--- app/src/main/resources/data.sql
+++ app/src/main/resources/data.sql
@@ -1,3 +1,7 @@
 INSERT INTO customer VALUES (1L, '2023-05-11 17:30:30', '2023-05-11 17:30:30', 20, 'p@gmail.com', 'Pedro', '3223549930', 'Alberto');
 INSERT INTO customer VALUES (2L, '2023-05-11 17:30:30', '2023-05-11 17:30:30', 80, 's@gmail.com', 'Sandro', '3203549142', 'Carta');
 INSERT INTO customer VALUES (3L, '2023-05-11 17:30:30', '2023-05-11 17:30:30', 56, 'c@gmail.com', 'Carlos', '3213746930', 'Castro');
+
+INSERT INTO address VALUES(1L, '2023-05-11 17:30:30', '2023-05-11 17:30:30', 'Cucuta', 'Colombia', 'Av 10 #24', 1L);
+INSERT INTO address VALUES(2L, '2023-05-11 17:30:30', '2023-05-11 17:30:30', 'Sao Pablo', 'Brasil', 'Calle 100 #40-4', 2L);
+INSERT INTO address VALUES(3L, '2023-05-11 17:30:30', '2023-05-11 17:30:30', 'La paz', 'Peru', 'Transversal 1a #2-2', 3L);
