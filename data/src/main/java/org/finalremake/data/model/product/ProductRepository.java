702771de371a08fb3163c8b1c86b651033bc6245
Kereser <Urregoarr00@gmail.com>
2023-05-11 11:57:35 -0500

    Adding ProductRepository

---
+++ data/src/main/java/org/finalremake/data/model/product/ProductRepository.java
@@ -0,0 +1,8 @@
+package org.finalremake.data.model.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
