885e32e7c62a1c6b849e77e24136adc88d4a6e20
Kereser <Urregoarr00@gmail.com>
2023-05-11 17:38:19 -0500

    Adding scan for repositories

--- app/src/main/java/org/finalremake/AppFinalApplication.java
+++ app/src/main/java/org/finalremake/AppFinalApplication.java
@@ -3,7 +3,7 @@ package org.finalremake;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.finalremake.data.model")
 // NECESSARY IF DATA IS IN PACKAGE DIFFERENT FROM ORG.FINALREMAKE in this case.
 //@EntityScan(basePackages = "org.finalremake.data.model")
 public class AppFinalApplication {
