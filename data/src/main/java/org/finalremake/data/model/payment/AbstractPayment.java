93b43fbd171c5b714044dbe0ec2ba488dfbbcbb7
Kereser <Urregoarr00@gmail.com>
2023-05-11 19:07:17 -0500

    Refactor abstractPayment

--- data/src/main/java/org/finalremake/data/model/payment/AbstractPayment.java
+++ data/src/main/java/org/finalremake/data/model/payment/AbstractPayment.java
@@ -1,29 +1,13 @@
 package org.finalremake.data.model.payment;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.validation.constraints.NotNull;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-
-import java.time.Instant;
+import org.finalremake.data.model.AbstractEntity;
 
 @MappedSuperclass
-public abstract class AbstractPayment implements IPayment {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
+public abstract class AbstractPayment extends AbstractEntity implements IPayment {
     @NotNull
     private String ACCOUNT_NUMBER;
-
-    @CreatedDate
-    private Instant createdAt;
-
-    @LastModifiedDate
-    private Instant updatedAt;
 
     @Override
     public String getAccountNumber() {
