21a4805b7b37a065aa9ef00f307505addbb8ffec
Kereser <Urregoarr00@gmail.com>
2023-05-11 19:10:06 -0500

    Adding ccpaymentResponseDTO

---
+++ data/src/main/java/org/finalremake/data/dto/payment/CreditCardPaymentResponseDTO.java
@@ -0,0 +1,13 @@
+package org.finalremake.data.dto.payment;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.finalremake.data.dto.AbstractDTO;
+
+@Builder
+@Getter
+@Setter
+public class CreditCardPaymentResponseDTO extends AbstractDTO {
+    private String ACCOUNT_NUMBER;
+}
