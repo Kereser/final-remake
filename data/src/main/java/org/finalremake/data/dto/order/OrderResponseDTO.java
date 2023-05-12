af4aa4de6b5abe74a4150eacc14297fa934bf913
Kereser <Urregoarr00@gmail.com>
2023-05-11 19:17:30 -0500

    Adding OrderResponseDTO

---
+++ data/src/main/java/org/finalremake/data/dto/order/OrderResponseDTO.java
@@ -0,0 +1,19 @@
+package org.finalremake.data.dto.order;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.finalremake.data.dto.AbstractDTO;
+import org.finalremake.data.dto.checkout.CheckoutResponseDTO;
+import org.finalremake.data.dto.delivery.DeliveryResponseDTO;
+import org.finalremake.data.dto.payment.PaymentResponseDTO;
+
+@Builder
+@Getter
+@Setter
+public class OrderResponseDTO extends AbstractDTO {
+    private CheckoutResponseDTO checkout;
+    private DeliveryResponseDTO delivery;
+    private PaymentResponseDTO payment;
+    private Boolean status;
+}
