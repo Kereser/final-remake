bd8ee250481c86939d7f556f392f0ce911450cf7
Kereser <Urregoarr00@gmail.com>
2023-05-19 10:26:15 -0500

    Adding address controller test

--- app/src/main/java/org/finalremake/AppFinalApplication.java
+++ app/src/main/java/org/finalremake/AppFinalApplication.java
@@ -3,7 +3,9 @@ package org.finalremake;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// This scanBasePackages annotation is used for repositories.
 @SpringBootApplication(scanBasePackages = "org.finalremake.data.model")
+
 // NECESSARY IF DATA IS IN PACKAGE DIFFERENT FROM ORG.FINALREMAKE in this case.
 //@EntityScan(basePackages = "org.finalremake.data.model")
 public class AppFinalApplication {
