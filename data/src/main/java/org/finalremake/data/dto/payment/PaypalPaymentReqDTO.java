c2a6865da74fe9e420ee256e08ec9fc2d4813e44
Kereser <Urregoarr00@gmail.com>
2023-05-18 21:23:22 -0500

    Adding PaypalPaymentReqDTO

---
+++ data/src/main/java/org/finalremake/data/dto/payment/PaypalPaymentReqDTO.java
@@ -0,0 +1,24 @@
+package org.finalremake.data.dto.payment;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Builder
+@Getter
+@Setter
+public class PaypalPaymentReqDTO {
+    @NotNull
+    @Size(min=16, max=16) private String ACCOUNT_NUMBER;
+
+    @NotNull
+    @Future
+    @DateTimeFormat(pattern = "MM/yyyy")
+    private Date goodTrough;
+}
