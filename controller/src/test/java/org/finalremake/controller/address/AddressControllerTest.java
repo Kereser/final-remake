92dfb9952fd894adabb65349e6199903c2441c18
Kereser <Urregoarr00@gmail.com>
2023-05-19 10:33:40 -0500

    Adding address controller test init

--- controller/src/test/java/org/finalremake/controller/address/AddressControllerTest.java
+++ controller/src/test/java/org/finalremake/controller/address/AddressControllerTest.java
@@ -1,7 +1,16 @@
 package org.finalremake.controller.address;
 
+import org.finalremake.service.address.AddressServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
 public class AddressControllerTest {
+    @MockBean
+    private AddressServiceImpl addressServiceImpl;
+    @Autowired
+    private MockMvc mockMvc;
+
 }
