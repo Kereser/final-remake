e2d0b28a97352455a5b1f1b19768b5b9b6f3aee7
Kereser <Urregoarr00@gmail.com>
2023-05-20 17:21:42 -0500

    Adding Delivery utils

---
+++ utils/src/main/java/org/finalremake/utils/DeliveryUtils.java
@@ -0,0 +1,54 @@
+package org.finalremake.utils;
+
+import org.finalremake.data.dto.delivery.DeliveryResponseDTO;
+import org.finalremake.data.model.delivery.Delivery;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Component
+public class DeliveryUtils {
+    private static Delivery getDelivery1() {
+        return Delivery.builder()
+                .id(1L)
+                .createdAt(Instant.now())
+                .updatedAt(Instant.now())
+                .address(AddressUtils.getAddress1())
+                .payment(PaymentUtils.getPayment1())
+                .totalAmount(1000.50)
+                .build();
+    }
+
+    private static Delivery getDelivery2() {
+        return Delivery.builder()
+                .id(2L)
+                .createdAt(Instant.now())
+                .updatedAt(Instant.now())
+                .address(AddressUtils.getAddress2())
+                .payment(PaymentUtils.getPayment2())
+                .totalAmount(2000.50)
+                .build();
+    }
+
+    public static DeliveryResponseDTO getDeliveryResponseDTO1() {
+        return DeliveryResponseDTO.builder()
+                .id(1L)
+                .createdAt(Instant.now())
+                .updatedAt(Instant.now())
+                .address(AddressUtils.getAddressResponseDTO1())
+                .payment(PaymentUtils.getPaymentResponseDTO1())
+                .totalAmount(1000.50)
+                .build();
+    }
+
+    public static DeliveryResponseDTO getDeliveryResponseDTO2() {
+        return DeliveryResponseDTO.builder()
+                .id(2L)
+                .createdAt(Instant.now())
+                .updatedAt(Instant.now())
+                .address(AddressUtils.getAddressResponseDTO2())
+                .payment(PaymentUtils.getPaymentResponseDTO2())
+                .totalAmount(2000.50)
+                .build();
+    }
+}
