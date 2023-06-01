2173c676b18856454bbdaff81e8733c7d9a37ee9
Kereser <Urregoarr00@gmail.com>
2023-05-31 20:43:19 -0500

    Adding updating mapper to address

--- data/src/main/java/org/finalremake/data/model/address/AddressMapper.java
+++ data/src/main/java/org/finalremake/data/model/address/AddressMapper.java
@@ -1,10 +1,16 @@
 package org.finalremake.data.model.address;
 
+import org.finalremake.data.dto.address.AddressReqUpdateDTO;
 import org.finalremake.data.dto.address.AddressResponseDTO;
 import org.finalremake.data.model.address.Address;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface AddressMapper {
     AddressResponseDTO getAddressResponseDTO(Address address);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Address getUpdatedAddress (AddressReqUpdateDTO addressReqUpdateDTO, @MappingTarget Address address);
 }
