013a7f65aed729ac2863ea5d70c47bbf71cb959b
Kereser <Urregoarr00@gmail.com>
2023-06-04 18:40:41 -0500

    Adding productServiceImpl frame

---
+++ service/src/main/java/org/finalremake/service/product/ProductService.java
@@ -0,0 +1,15 @@
+package org.finalremake.service.product;
+
+import org.finalremake.data.dto.product.ProductReqDTO;
+import org.finalremake.data.dto.product.ProductReqUpdateDTO;
+import org.finalremake.data.dto.product.ProductResponseDTO;
+
+import java.util.List;
+
+public interface ProductService {
+    void deleteProduct(Long productId);
+    ProductResponseDTO createProduct(ProductReqDTO productReqDTO);
+    ProductResponseDTO getProduct(Long productId);
+    List<ProductResponseDTO> getProducts();
+    ProductResponseDTO updateProduct(ProductReqUpdateDTO productReqUpdateDTO);
+}
---
+++ service/src/main/java/org/finalremake/service/product/ProductServiceImpl.java
@@ -0,0 +1,36 @@
+package org.finalremake.service.product;
+
+import org.finalremake.data.dto.product.ProductReqDTO;
+import org.finalremake.data.dto.product.ProductReqUpdateDTO;
+import org.finalremake.data.dto.product.ProductResponseDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public void deleteProduct(Long productId) {
+
+    }
+
+    @Override
+    public ProductResponseDTO createProduct(ProductReqDTO productReqDTO) {
+        return null;
+    }
+
+    @Override
+    public ProductResponseDTO getProduct(Long productId) {
+        return null;
+    }
+
+    @Override
+    public List<ProductResponseDTO> getProducts() {
+        return null;
+    }
+
+    @Override
+    public ProductResponseDTO updateProduct(ProductReqUpdateDTO productReqUpdateDTO) {
+        return null;
+    }
+}
