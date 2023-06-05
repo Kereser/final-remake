b9c3502f9c2aa32be726e40869764d138a78f514
Kereser <Urregoarr00@gmail.com>
2023-06-05 15:22:11 -0500

    Adding checkout service frame

---
+++ service/src/main/java/org/finalremake/service/checkout/CheckoutService.java
@@ -0,0 +1,12 @@
+package org.finalremake.service.checkout;
+
+import org.finalremake.data.dto.checkout.CheckoutResponseDTO;
+
+import java.util.Map;
+
+public interface CheckoutService {
+    CheckoutResponseDTO createCheckout(Long customerId, Map<Long, Integer> products);
+    void deleteCheckout(Long checkoutId);
+    CheckoutResponseDTO getCheckout(Long checkoutId);
+    CheckoutResponseDTO updateCheckout(Long checkoutId, Map<String, String> allParams);
+}
