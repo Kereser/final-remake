8ba880ca1d871d9aa2987a6218e14337c1556b6a
Kereser <Urregoarr00@gmail.com>
2023-06-05 15:22:38 -0500

    Adding checkout service frame

---
+++ service/src/main/java/org/finalremake/service/checkout/CheckoutServiceImpl.java
@@ -0,0 +1,29 @@
+package org.finalremake.service.checkout;
+
+import org.finalremake.data.dto.checkout.CheckoutResponseDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class CheckoutServiceImpl implements CheckoutService {
+    @Override
+    public CheckoutResponseDTO createCheckout(Long customerId, Map<Long, Integer> products) {
+        return null;
+    }
+
+    @Override
+    public void deleteCheckout(Long checkoutId) {
+
+    }
+
+    @Override
+    public CheckoutResponseDTO getCheckout(Long checkoutId) {
+        return null;
+    }
+
+    @Override
+    public CheckoutResponseDTO updateCheckout(Long checkoutId, Map<String, String> allParams) {
+        return null;
+    }
+}
