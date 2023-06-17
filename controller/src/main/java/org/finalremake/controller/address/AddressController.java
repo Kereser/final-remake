c94e212032be91e7e56cadfc87deb570e1d29fce
Kereser <Urregoarr00@gmail.com>
2023-06-16 22:06:08 -0500

    Adding Address Controller impl

--- controller/src/main/java/org/finalremake/controller/address/AddressController.java
+++ controller/src/main/java/org/finalremake/controller/address/AddressController.java
@@ -1,8 +1,13 @@
 package org.finalremake.controller.address;
 
+import org.finalremake.data.dto.address.AddressReqDTO;
+import org.finalremake.data.dto.address.AddressReqUpdateDTO;
+import org.finalremake.data.dto.address.AddressResponseDTO;
 import org.finalremake.service.address.AddressServiceImpl;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/addresses")
@@ -12,4 +17,29 @@ public class AddressController {
     public AddressController(AddressServiceImpl addressServiceImpl) {
         this.addressServiceImpl = addressServiceImpl;
     }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public AddressResponseDTO getAddress(@PathVariable Long id) {
+        return addressServiceImpl.getAddress(id);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<AddressResponseDTO> getAddresses() {
+        return addressServiceImpl.getAddresses();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteAddress(@PathVariable Long id) {
+        addressServiceImpl.deleteAddress(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public AddressResponseDTO updateAddress(@PathVariable Long id, @RequestBody AddressReqUpdateDTO addressReqUpdateDTO) {
+        return addressServiceImpl.updateAddress(addressReqUpdateDTO, id);
+    }
 }
+
