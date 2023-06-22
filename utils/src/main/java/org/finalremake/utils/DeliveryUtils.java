16021e2aa7ecafad74963f9282e7e364ef342006
Kereser <Urregoarr00@gmail.com>
2023-06-22 15:01:23 -0500

    Adding verification for delivery properties

--- utils/src/main/java/org/finalremake/utils/DeliveryUtils.java
+++ utils/src/main/java/org/finalremake/utils/DeliveryUtils.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.AbstractMap;
 import java.util.Map;
 import java.util.Optional;
 
@@ -65,7 +66,6 @@ public class DeliveryUtils {
     }
 
     public Delivery createDelivery(Checkout checkout) {
-
         return Delivery.builder()
                 .createdAt(Instant.now())
                 .updatedAt(Instant.now())
@@ -82,5 +82,11 @@ public class DeliveryUtils {
             totalAmount += p.map(Product::getPrice).orElse(0.0);
         }
         return totalAmount;
+    }
+
+    public Map.Entry<String, String> isValidDelivery(Delivery delivery) {
+        if (delivery.getAddress() == null) return new AbstractMap.SimpleEntry<>("Address", "Address is required");
+        else if (delivery.getPayment() == null) return new AbstractMap.SimpleEntry<>("Payment", "Payment is required");
+        else return new AbstractMap.SimpleEntry<>(null, null);
     }
 }
